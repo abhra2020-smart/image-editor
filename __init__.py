ded1eeffd3be3f07bbcd24b67544c7b5eb548714
abhra2020-smart <68565872+abhra2020-smart@users.noreply.github.com>
2020-10-05 15:59:16 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,17 @@
+__counter = 0
+
+def suml(list):
+	global __counter
+	__counter += 1
+	sum = 0
+	for el in list:
+		sum += el
+	return sum
+
+def prodl(list):
+	global __counter	
+	__counter += 1
+	prod = 1
+	for el in list:
+		prod *= el
+	return prod
